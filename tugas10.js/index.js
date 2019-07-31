cf72e955f83cdd2f65467b9bcf9a2179417bf80e
Engkos <engkoskostaman97@gmail.com>
2019-08-01 02:15:43 +0700

    tugas 10 js
    tugas ini kami kumpulkan untuk nioic

---
+++ tugas10.js/index.js
@@ -0,0 +1,23 @@
+function pengulangan(){
+var i=1;i<=50;
+console.log(i)
+i+=2;
+console.log(i)
+i+=3;
+console.log(i)
+i+=4;
+console.log(i)
+i+=5;
+console.log(i)
+i+=6;
+console.log(i)
+i+=7;
+console.log(i)
+i+=8;
+console.log(i)
+i+=9;
+console.log(i)
+i+=10;
+console.log(i)
+}
+pengulangan()
